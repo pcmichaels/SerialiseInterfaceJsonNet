b9c608f8f56dc40a5d7d0a76459fa5c0166d65c2
pcmichaels <pcmichaels@live.com>
2017-05-09 07:24:11 +0100

    Add serialisation demonstration

--- SerialiseInterfaceJsonNet/Program.cs
+++ SerialiseInterfaceJsonNet/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,33 @@ namespace SerialiseInterfaceJsonNet
     {
         static void Main(string[] args)
         {
+            string json = SerialiseProduct();
+            Console.WriteLine(json);
+
+            IProduct product = DeserialiseProduct(json);
+            Console.WriteLine(product.Id);
+            
+            Console.ReadLine();
+
+        }
+
+        private static IProduct DeserialiseProduct(string json)
+        {
+            IProduct product = JsonConvert.DeserializeObject<IProduct>(json);
+
+            return product;
+        }
+
+        private static string SerialiseProduct()
+        {
+            IProduct product = new Product()
+            {
+                Id = 1,
+                UnitPrice = 12.3m
+            };
+
+            string json = JsonConvert.SerializeObject(product);
+            return json;
         }
     }
 }
